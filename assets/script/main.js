1f644e2679c7f8f432b1cec46f519ba794990c73
Ian Alves Sousa <iansousa.dev@gmail.com>
2024-10-27 00:50:15 -0300

    commit

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,12 +1,22 @@
 const toggleTheme = document.getElementById("toggle-theme");
+const menulinks = document.querySelectorAll(".menu_link");
 
-
-function changeTheme(){
-    toggleTheme.classList.toggle("bi-sun")
-    toggleTheme.classList.toggle("bi-moon")
+// Função de troca de tema
+function changeTheme() {
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon");
 
     const html = document.getElementsByTagName("html")[0];
     html.dataset.theme = html.dataset.theme === "light" ? "dark" : "light";
-};
+}
 
-toggleTheme.addEventListener("click", changeTheme)
+// Adiciona a classe "active" aos links do menu
+menulinks.forEach(element => {
+    element.addEventListener("click", () => {
+        menulinks.forEach(i => i.classList.remove("active"));
+        element.classList.add("active");
+    });
+});
+
+// Chama a função de troca de tema
+toggleTheme.addEventListener("click", changeTheme);
